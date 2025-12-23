b19032728ee57137fd0a23f24d775ccddd72d432
chile-kasoka <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>
2025-12-23 12:54:08 +0200

    server js port

--- server/server.js
+++ server/server.js
@@ -175,6 +175,6 @@ app.get("/", (req, res) => {
   res.json({ message: "API is running..." });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on ${PORT}`);
 });
