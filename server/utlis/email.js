fd4ff41792c7623b3104ed93e8b53f09a582fce2
chile-kasoka <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>
2026-01-08 14:27:13 +0200

    attendees fix

--- server/utlis/email.js
+++ server/utlis/email.js
@@ -1,11 +1,11 @@
-import { Resend } from "resend";
+import * as ResendLib from "resend"; // import everything
 import dotenv from "dotenv";
 import path from "path";
 
-// Explicitly load .env from project root
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// Instantiate Resend using the named export
+const resend = new ResendLib.Resend(process.env.RESEND_API_KEY);
 
 export const SendEmail = async ({ to, subject, html }) => {
   if (!process.env.RESEND_API_KEY) {
@@ -14,11 +14,12 @@ export const SendEmail = async ({ to, subject, html }) => {
 
   try {
     const response = await resend.emails.send({
-      from: process.env.RESEND_FROM_EMAIL,
+      from: process.env.RESEND_FROM_EMAIL, // make sure this is set in .env
       to,
       subject,
       html,
     });
+
     console.log("Email sent:", response);
     return response;
   } catch (err) {
