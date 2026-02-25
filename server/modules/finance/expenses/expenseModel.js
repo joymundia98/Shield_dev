1aa48a679f1c2192ad1ee615341608f974f0ce70
chile-kasoka <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>
2026-02-25 14:46:16 +0200

    fix

--- server/modules/finance/expenses/expenseModel.js
+++ server/modules/finance/expenses/expenseModel.js
@@ -102,7 +102,7 @@ const Expense = {
     const result = await pool.query(
       `UPDATE expenses
        SET status = $1
-       WHERE id = $2 AND organization_id=$2
+       WHERE id = $2 AND organization_id=$3
        RETURNING *`,
       [status, id, organization_id]
     );
